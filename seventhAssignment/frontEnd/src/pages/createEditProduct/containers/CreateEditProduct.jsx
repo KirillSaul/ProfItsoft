c965a94eb433ff7a330ef27943df2dc701260673
Kirill Saulyak <kerich345@gmail.com>
2023-02-15 20:24:49 +0200

    with security

--- seventhAssignment/frontEnd/src/pages/createEditProduct/containers/CreateEditProduct.jsx
+++ seventhAssignment/frontEnd/src/pages/createEditProduct/containers/CreateEditProduct.jsx
@@ -1,18 +1,19 @@
-import {getJson, postJson} from "../../../requests";
 import {useEffect, useState} from "react";
-import {useLocation, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import Button from 'components/Button';
-import Box from 'components/Box';
-import Grid from 'components/Grid';
-import InputName from "./components/InputName";
 import TextField from "components/TextField";
 import {useDispatch, useSelector} from "react-redux";
 import productActions from "../store/actions/actions";
+import useAccessValidate from "../../../hooks/useAccessValidate";
 
 const CreateEditProduct = ({authorities}) => {
     const initialState = {id: null, name: null, categoryId: null}
     const [newProduct, setNewProduct] = useState(initialState);
 
+    const canSeeList = useAccessValidate({
+        ownedAuthorities: authorities,
+        neededAuthorities: ['МОЖНО_ВОТ_ЭТУ_ШТУКУ'],
+    });
 
     const dispatch = useDispatch();
 
@@ -33,24 +34,30 @@ const CreateEditProduct = ({authorities}) => {
     }, [product])
 
     return (
-        <div>
-            <div>name:</div>
-            <div>
 
-                <TextField value={newProduct.name !== null ? newProduct.name : ""} onChange={(e) => {
-                    setNewProduct((prevState) => ({...prevState, name: e.target.value}))
-                }}></TextField>
-            </div>
-            <div>categoryId:</div>
-            <div>
-                <TextField value={newProduct.categoryId !== null ? newProduct.categoryId : ""} onChange={(e) => {
-                    setNewProduct((prevState) => ({...prevState, categoryId: e.target.value}))
-                }}></TextField>
-            </div>
-            <Button onClick={() => {
-                productId === undefined? dispatch(productActions.postProduct(newProduct)): dispatch(productActions.putProduct(newProduct))
-            }}>Save</Button>
+        <div>
+            {canSeeList &&
+                <div>
+                    <div>name:</div>
+                    <div>
+
+                        <TextField value={newProduct.name !== null ? newProduct.name : ""} onChange={(e) => {
+                            setNewProduct((prevState) => ({...prevState, name: e.target.value}))
+                        }}></TextField>
+                    </div>
+                    <div>categoryId:</div>
+                    <div>
+                        <TextField value={newProduct.categoryId !== null ? newProduct.categoryId : ""}
+                                   onChange={(e) => {
+                                       setNewProduct((prevState) => ({...prevState, categoryId: e.target.value}))
+                                   }}></TextField>
+                    </div>
+                    <Button onClick={() => {
+                        productId === undefined ? dispatch(productActions.postProduct(newProduct)) : dispatch(productActions.putProduct(newProduct))
+                    }}>Save</Button>
+                </div>}
         </div>
+
     )
 
 }
--- seventhAssignment/frontEnd/src/pages/productList/containers/ProductList.jsx
+++ seventhAssignment/frontEnd/src/pages/productList/containers/ProductList.jsx
@@ -1,15 +1,19 @@
-import {postJson} from "../../../requests";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Button from 'components/Button';
 import Box from 'components/Box';
 import Grid from 'components/Grid';
 import Link from 'components/Link'
 import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
 import productActions from "../../productList/store/actions/actions";
+import useAccessValidate from "../../../hooks/useAccessValidate";
 
 const ProductList = ({authorities}) => {
     const [productList, setProductList] = useState([]);
+
+    const canSeeList = useAccessValidate({
+        ownedAuthorities: authorities,
+        neededAuthorities: ['МОЖНО_ВОТ_ЭТУ_ШТУКУ'],
+    });
 
     const dispatch = useDispatch();
     useEffect(() => {
@@ -28,29 +32,33 @@ const ProductList = ({authorities}) => {
 
     return (
         <div>
-            <Button variant="contained" color="primary" href="/createEditProduct">
-                Create
-            </Button>
-            <div> {
-                productList.map(value =>
-                    (
-                        <Grid>
-                            <Box sx={{m: 2}}/>
-                            {value.name}
-                            <Link to={"/createEditProduct/" + value.id }>
-                                <Button variant="contained" color="primary">
-                                    Edit
-                                </Button>
-                            </Link>
-                            <Button variant="contained" color="secondary" onClick={()=>dispatch(productActions.deleteProductById(value.id))}>
-                                Delete
-                            </Button>
-
-                        </Grid>
-
-                    )
-                )
-            } </div>
+            {canSeeList &&
+                <div>
+                    <Button variant="contained" color="primary" href="/createEditProduct">
+                        Create
+                    </Button>
+                    <div> {
+                        productList.map(value =>
+                            (
+                                <Grid>
+                                    <Box sx={{m: 2}}/>
+                                    {value.name}
+                                    <Link to={"/createEditProduct/" + value.id}>
+                                        <Button variant="contained" color="primary">
+                                            Edit
+                                        </Button>
+                                    </Link>
+                                    <Button variant="contained" color="secondary"
+                                            onClick={() => dispatch(productActions.deleteProductById(value.id))}>
+                                        Delete
+                                    </Button>
+                                </Grid>
+                            )
+                        )
+                    }
+                    </div>
+                </div>
+            }
         </div>
     )
 
